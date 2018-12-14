5b7ef45e7bb5021eda1aa6af6ee571c6e2da2733
Megan Tee <183678E@mymail.nyp.edu.sg>
2018-12-14 21:36:57 +0800

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,17 @@
+from flask import *
+from jinja2 import TemplateNotFound
+
+auth = Blueprint("auth", __name__, template_folder='templates', url_prefix='/auth')
+
+
+@auth.route('/')
+def homepage():
+    return render_template("endlesspages.html")
+
+@auth.route('/login')
+def login():
+    return render_template("login.html")
+
+@auth.route('/register')
+def register():
+    return render_template("signup.html")
