47a18acf7579f5ccae5701034258cbf214c247dc
Megan Tee <183678E@mymail.nyp.edu.sg>
2019-05-07 15:45:18 +0800

    Enter

--- flaskr.sql
+++
@@ -1,8 +0,0 @@
-DROP Table if exists Profile;
-
-Create Table if not exists Profile  (
-    Name  TEXT PRIMARY KEY,
-    Email Text NOT NULL UNIQUE,
-    Password TEXT NOT NULL,
-    Age INTEGER NOT NULL
-)
